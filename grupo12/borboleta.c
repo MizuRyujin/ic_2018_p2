f3335afe9692b209b36bed38d626cfb9a6f8910c
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-01-07 17:25:10 +0000

    criação de borboleta.h (display) acabar borboleta.c e borboleta.h

--- grupo12/borboleta.c
+++ grupo12/borboleta.c
@@ -3,17 +3,6 @@
 #include "player.h"
 //#include "borboleta.h"
 
-#define MAX 125
-
-char map [MAX][MAX];
-int north = 1;
-int south = 1;
-int east = 1;
-int west = 1;
-char village = 1;
-char city = 1;
-char player = 1;
-
 int main()
 {
   for (int i = 0; i < ydim; i++);
---
+++ grupo12/borboleta.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "player.h"
+
+#define MAX 125
+
+char map [MAX][MAX];
+int north = 1;
+int south = 1;
+int east = 1;
+int west = 1;
+char village = 1;
+char city = 1;
+char player = 1;
