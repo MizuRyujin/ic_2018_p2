d08f142544103341f64b2d30fe960eb5d5bfd9d5
Eggo <miguelrf95@gmail.com>
2019-01-07 18:27:26 +0000

    Mapa.h voltou para a primeira versão com menos items na estrutura e comecei a definir o header para o display do mapa em ASCII

--- grupo12/Mapa.h
+++ grupo12/Mapa.h
@@ -4,26 +4,16 @@
 typedef void *ITEM;
 
 typedef struct {
-	enum {desert; iron; brick; wool; lumber; grain};
-	int number;
-	} TERRAIN;
-
-typedef struct {
-	TERRAIN north, south, east, west;
-	int player
-	enum
-
-typedef struct {
 	ITEM*map;
 	unsigned int xdim;
 	unsigned int ydim;
     } MAPA;
 
-MAPA*create_mapa(unsigned int xdim, unsigned int ydim, unsigned char north, unsigned char south, unsigned char east, unsigned char west);
+MAPA*create_mapa(unsigned int xdim, unsigned int ydim);
 void destroy_map(MAPA*mp);
 void destroy_map_full(MAPA*mp, void(item_destroy)(ITEM*item));
 
-void mapa_put(MAPA*mp, unsigned int x, unsigned int y, unsigned char north, unsigned char south, unsigned char east, unsigned char west, ITEM*item);
-ITEM*mapa_get(MAPA*mp, unsigned int x, unsigned int y, unsigned char north, unsigned char south, unsigned char east, unsigned char west);
+void mapa_put(MAPA*mp, unsigned int x, unsigned int y, ITEM*item);
+ITEM*mapa_get(MAPA*mp, unsigned int x, unsigned int y);
 
 #endif
--- grupo12/borboleta.h
+++ grupo12/borboleta.h
@@ -1,7 +1,24 @@
+#ifndef __BORBOLETA_H__
+#define __BORBOLETA_H__
+
 #include <stdio.h>
 #include "player.h"
 
 #define MAX 125
+
+typedef struct {
+	enum {desert; iron; brick; wool; grain; lumber};
+	int coordinate;
+	}TERRAIN;
+
+typedef struct {
+	TERRAIN north, south, east, west;
+	int player
+	enum {nome; aldeia; cidade};
+	}NODE;
+
+
+
 
 char map [MAX][MAX];
 int north = 1;
