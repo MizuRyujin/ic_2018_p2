7f2b0976c245806c82af177760d82371e9ced08d
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-01-07 16:03:54 +0000

    tradução do mapa.c

--- grupo12/Mapa.c
+++ grupo12/Mapa.c
@@ -4,16 +4,16 @@
 #include "abrirINI.h"
 #include <stdlib.h>
 
-MAPA*create_mapa(unsigned int xdim, unsigned int ydim, unsigned char norte, unsigned char sul, unsigned char este, unsigned char oeste){
+MAPA*create_mapa(unsigned int xdim, unsigned int ydim, unsigned char north, unsigned char south, unsigned char east, unsigned char west){
     MAPA*mp = NULL;
     mp = malloc(sizeof(MAPA));
     mp ->map = calloc(xdim*ydim, sizeof(ITEM));
     mp -> xdim = xdim;
     mp -> ydim = ydim;
-    mp -> norte = norte;
-    mp -> sul = sul;
-    mp -> este = este;
-    mp -> oeste = oeste;
+    mp -> north = north;
+    mp -> south = south;
+    mp -> east = east;
+    mp -> west = west;
     return mp;
 }
 
@@ -23,7 +23,7 @@ void destroy_mapa(MAPA*mp){
 }
 
 void destroy_mapa_full(MAPA*mp, void(*item_destroy)(ITEM*item)){
-    for(unsigned int i = 0; i *mp -> xdim *mp -> ydim *mp -> norte *mp -> sul *mp -> este *mp -> oeste; ++i){
+    for(unsigned int i = 0; i *mp -> xdim *mp -> ydim *mp -> north *mp -> south *mp -> east *mp -> west; ++i){
         if (mp -> map[i] !=NULL){
             item_destroy(mp ->map[i]);
         }
@@ -31,11 +31,11 @@ void destroy_mapa_full(MAPA*mp, void(*item_destroy)(ITEM*item)){
     destroy_mapa(mp);
 }
 
-void mapa_put(MAPA*mp, unsigned int x, unsigned int y, unsigned char norte, unsigned char sul, unsigned char este, unsigned char oeste, ITEM*item){
+void mapa_put(MAPA*mp, unsigned int x, unsigned int y, unsigned char north, unsigned char south, unsigned char east, unsigned char west, ITEM*item){
     mp -> map [y*mp -> xdim + x] = item;
 }
 
-ITEM*mapa_get(MAPA*mp, unsigned int x, unsigned int y, unsigned char norte, unsigned char sul, unsigned char este, unsigned char oeste) {
+ITEM*mapa_get(MAPA*mp, unsigned int x, unsigned int y, unsigned char north, unsigned char south, unsigned char east, unsigned char west) {
     return mp -> map [y* mp -> xdim + x];
 }
 
