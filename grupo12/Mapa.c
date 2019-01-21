21d810682b5af23a78a68bdca88f28b253af3542
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-01-21 17:41:13 +0000

    comentários relativos ao doxyfile nos ficheiros Mapa.c e .h, abrirINI.c e .h e catanSimples.c

--- grupo12/Mapa.c
+++ grupo12/Mapa.c
@@ -4,6 +4,9 @@
 #include "abrirINI.h"
 #include <stdlib.h>
 
+/**
+ * definir estrutura do mapa e coordenadas do mesmo
+ * */
 MAPA *create_mapa(unsigned int xdim, unsigned int ydim){
     MAPA *mp = NULL;
     mp = malloc(sizeof(MAPA));
@@ -13,6 +16,9 @@ MAPA *create_mapa(unsigned int xdim, unsigned int ydim){
     return mp;
 }
 
+/**
+* funções de destruição do mapa
+* */
 void destroy_mapa(MAPA *mp){
     free(mp->map);
     free(mp);
@@ -26,7 +32,9 @@ void destroy_mapa_full(MAPA *mp, void(*item_destroy)(ITEM *item)){
     }
     destroy_mapa(mp);
 }
-
+/**
+* funções da criação do mapa
+* */
 void mapa_put(MAPA *mp, unsigned int x, unsigned int y, ITEM *item){
     mp -> map [y * mp -> xdim + x] = item;
 }
--- grupo12/abrirINI.c
+++ grupo12/abrirINI.c
@@ -5,17 +5,15 @@
 
 #include "abrirINI.h"
 
+/**
+ * chama função para abrir o ficheiro de config.
+ * e erro correspondente
+ * */
 FILE * abreINI(char * nome, char * modo)
 {
 	FILE * f;
 	printf("A abrir ficheiro %s\n", nome);
 	f = fopen(nome, modo);
-
-	//void delay(unsigned int sec)
-	//{
-	//	unsigned int retTime = time(0) + sec;
-	//	while(time(0) < retTime);
-	//}
 
 	if(f == NULL)
 	{
