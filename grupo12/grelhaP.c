c545fa365aeac6677839dbca08c9a0f5ba3bc38b
Nuno <nuno27galego@gmail.com>
2019-01-22 18:59:00 +0000

    grelha

--- grupo12/grelhaP.c
+++ grupo12/grelhaP.c
@@ -1,60 +1,60 @@
 #include <stdio.h>
 #include <string.h>
 #include "player1.h"
+#include "grelhaP.h"
 
 NODE node = {1,0};
 
 int grelha()
 {
-	printf("+------------+------------+------------+------------+\n");
+        printf("+------------+------------+------------+------------+\n");
         printf("+            +            +            +            +\n");
-        printf("+    #=%d    +    #=%d    +    #=%d    +    #=%d    +\n", node.coordinates, node.coordinates+=1, node.coordinates+=2, node.coordinates+=3);
+        printf("+    #=0%d    +    #=0%d    +    #=0%d    +    #=0%d    +\n", node.coordinates, node.coordinates+1, node.coordinates+2, node.coordinates+3);
         printf("+            +            +            +            +\n");
-        printf("+    N=      +            +            +            +\n");
-	printf("+    W=      +            +            +            +\n");
-        printf("+    E=      +            +            +            +\n");
-        printf("+    S=      +            +            +            +\n");
+        printf("+    N=      +    N=      +    N=      +    N=      +\n");
+        printf("+    W=      +    W=      +    W=      +    W=      +\n");
+        printf("+    E=      +    E=      +    E=      +    E=      +\n");
+        printf("+    S=      +    S=      +    S=      +    S=      +\n");
         printf("+            +            +            +            +\n");
         printf("+            +            +            +            +\n");
-	printf("+            +            +            +            +\n");
+        printf("+            +            +            +            +\n");
         printf("+            +            +            +            +\n");
         printf("+------------+------------+------------+------------+\n");
         printf("+            +            +            +            +\n");
-        printf("+    #=%d    +    #=%d    +    #=%d    +    #=%d    +\n", node.coordinates+=4, node.coordinates+=5, node.coordinates+=6, node.coordinates+=7);
+        printf("+    #=0%d    +    #=0%d    +    #=0%d    +    #=0%d    +\n", node.coordinates+4, node.coordinates+5, node.coordinates+6, node.coordinates+7);
         printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-	printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-	printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-	printf("+------------+------------+------------+------------+\n");
-        printf("+            +            +            +            +\n");
-        printf("+    #=%d    +    #=%d    +    #=%d    +    #=%d    +\n", node.coordinates+=8, node.coordinates+=9, node.coordinates+=10, node.coordinates+=11);
-        printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-	printf("+            +            +            +            +\n");
+        printf("+    N=      +    N=      +    N=      +    N=      +\n");
+        printf("+    W=      +    W=      +    W=      +    W=      +\n");
+        printf("+    E=      +    E=      +    E=      +    E=      +\n");
+        printf("+    S=      +    S=      +    S=      +    S=      +\n");
         printf("+            +            +            +            +\n");
         printf("+            +            +            +            +\n");
         printf("+            +            +            +            +\n");
         printf("+            +            +            +            +\n");
-	printf("+            +            +            +            +\n");
+        printf("+------------+------------+------------+------------+\n");
         printf("+            +            +            +            +\n");
-	printf("+------------+------------+------------+------------+\n");
+        printf("+    #=0%d    +    #=%d    +    #=%d    +    #=%d    +\n", node.coordinates+8, node.coordinates+9, node.coordinates+10, node.coordinates+11);
         printf("+            +            +            +            +\n");
-        printf("+    #=%d    +    #=%d    +    #=%d    +    #=%d    +\n", node.coordinates+=12, node.coordinates+=13, node.coordinates+=14, node.coordinates+=15);
-        printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-	printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
-        printf("+            +            +            +            +\n");
+        printf("+    N=      +    N=      +    N=      +    N=      +\n");
+        printf("+    W=      +    W=      +    W=      +    W=      +\n");
+        printf("+    E=      +    E=      +    E=      +    E=      +\n");
+        printf("+    S=      +    S=      +    S=      +    S=      +\n");
         printf("+            +            +            +            +\n");
         printf("+            +            +            +            +\n");
-	printf("+            +            +            +            +\n");
+        printf("+            +            +            +            +\n");
+        printf("+            +            +            +            +\n");
+        printf("+------------+------------+------------+------------+\n");
+        printf("+            +            +            +            +\n");
+        printf("+    #=%d    +    #=%d    +    #=%d    +    #=%d    +\n", node.coordinates+12, node.coordinates+13, node.coordinates+14, node.coordinates+15);
+        printf("+            +            +            +            +\n");
+        printf("+    N=      +    N=      +    N=      +    N=      +\n");
+        printf("+    W=      +    W=      +    W=      +    W=      +\n");
+        printf("+    E=      +    E=      +    E=      +    E=      +\n");
+        printf("+    S=      +    S=      +    S=      +    S=      +\n");
+        printf("+            +            +            +            +\n");
+        printf("+            +            +            +            +\n");
+        printf("+            +            +            +            +\n");
         printf("+            +            +            +            +\n");
         printf("+------------+------------+------------+------------+\n");
 }
-
 
